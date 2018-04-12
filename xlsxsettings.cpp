3441cd38693339a58a87032d3713563b78f13d3b
Luca Tringali <tringalinvent@libero.it>
2018-04-12 15:42:36 +0200

    XLSX settings dialog ui

---
+++ xlsxsettings.cpp
@@ -0,0 +1,102 @@
+#include "xlsxsettings.h"
+#include "ui_xlsxsettings.h"
+
+xlsxsettings::xlsxsettings(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::xlsxsettings)
+{
+    ui->setupUi(this);
+}
+
+xlsxsettings::~xlsxsettings()
+{
+    delete ui;
+}
+
+void xlsxsettings::set_ID(QStringList options, int selected) {
+    ui->xs_ID->addItems(options);
+    ui->xs_ID->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_ID() {
+    return ui->xs_ID->currentIndex()+1;
+}
+
+void xlsxsettings::set_surname(QStringList options, int selected) {
+    ui->xs_surname->addItems(options);
+    ui->xs_surname->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_surname() {
+    return ui->xs_surname->currentIndex()+1;
+}
+
+void xlsxsettings::set_name(QStringList options, int selected) {
+    ui->xs_name->addItems(options);
+    ui->xs_name->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_name() {
+    return ui->xs_name->currentIndex()+1;
+}
+
+void xlsxsettings::set_curricula(QStringList options, int selected) {
+    ui->xs_curricula->addItems(options);
+    ui->xs_curricula->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_curricula() {
+    return ui->xs_curricula->currentIndex()+1;
+}
+
+void xlsxsettings::set_year(QStringList options, int selected) {
+    ui->xs_year->addItems(options);
+    ui->xs_year->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_year() {
+    return ui->xs_year->currentIndex()+1;
+}
+
+void xlsxsettings::set_priority(QStringList options, int selected) {
+    ui->xs_priority->addItems(options);
+    ui->xs_priority->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_priority() {
+    return ui->xs_priority->currentIndex()+1;
+}
+
+void xlsxsettings::set_dest(QStringList options, int selected) {
+    ui->xs_dest->addItems(options);
+    ui->xs_dest->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_dest() {
+    return ui->xs_dest->currentIndex()+1;
+}
+
+void xlsxsettings::set_lslm(QStringList options, int selected) {
+    ui->xs_lslm->addItems(options);
+    ui->xs_lslm->setCurrentIndex(selected-1);
+}
+
+int xlsxsettings::get_lslm() {
+    return ui->xs_lslm->currentIndex()+1;
+}
+
+void xlsxsettings::set_lm(QString text) {
+    ui->xs_lm->setText(text);
+}
+
+QString xlsxsettings::get_lm() {
+    return ui->xs_lm->text();
+}
+
+bool xlsxsettings::get_addcities() {
+    return ui->xs_addcities->isChecked();
+}
+
+bool xlsxsettings::get_requisites() {
+    return ui->xs_addcities->isChecked();
+}
---
+++ xlsxsettings.h
@@ -0,0 +1,52 @@
+#ifndef XLSXSETTINGS_H
+#define XLSXSETTINGS_H
+
+#include <QDialog>
+
+namespace Ui {
+class xlsxsettings;
+}
+
+class xlsxsettings : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit xlsxsettings(QWidget *parent = 0);
+    ~xlsxsettings();
+
+    void set_ID(QStringList options, int selected);
+    int get_ID();
+
+    void set_surname(QStringList options, int selected);
+    int get_surname();
+
+    void set_name(QStringList options, int selected);
+    int get_name();
+
+    void set_curricula(QStringList options, int selected);
+    int get_curricula();
+
+    void set_year(QStringList options, int selected);
+    int get_year();
+
+    void set_priority(QStringList options, int selected);
+    int get_priority();
+
+    void set_dest(QStringList options, int selected);
+    int get_dest();
+
+    void set_lslm(QStringList options, int selected);
+    int get_lslm();
+
+    void set_lm(QString text);
+    QString get_lm();
+
+    bool get_addcities();
+    bool get_requisites();
+
+private:
+    Ui::xlsxsettings *ui;
+};
+
+#endif // XLSXSETTINGS_H
